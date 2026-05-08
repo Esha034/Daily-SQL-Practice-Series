5bd8272e8e8307b3804908f471c669c9de3dfe5a
ESHANI BANIK <banikesha03@gmail.com>
2026-05-08 22:16:38 +0530

    Create Union for Combination Information.sql

---
+++ 6. Combing Data(using SET)/Union for Combination Information.sql
@@ -0,0 +1,40 @@
+--Combine all the orders from 2 seperate tables(orders and orderarchive) 
+--into 1 report without duplicates 
+
+
+SELECT 
+
+'Orders' AS SourceTable
+,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+
+UNION
+
+SELECT  
+'OrdersArchive' AS SourceTable
+
+,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
+ORDER BY OrderID
